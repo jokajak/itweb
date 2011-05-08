6c7d19144800d7f825fb5ebe00e172be07bf4a41
Josh <jokajak@gmail.com>
2011-05-07 22:34:41 -0400

    include components as a model and fix it

--- libs/itweb/itweb/model/component.py
+++ libs/itweb/itweb/model/component.py
@@ -5,11 +5,11 @@ from sqlalchemy import *
 from sqlalchemy.orm import mapper, relation
 from sqlalchemy import Table, ForeignKey, Column
 from sqlalchemy.types import Integer, Unicode
-#from sqlalchemy.orm import relation, backref
+from sqlalchemy.orm import relation, backref
 
 from itweb.model import DeclarativeBase, metadata, DBSession
 
-class ComponentTypeModel(DeclarativeBase):
+class ComponentType(DeclarativeBase):
     __tablename__ = 'componenttypes'
 
     #{ Columns
@@ -20,14 +20,14 @@ class ComponentTypeModel(DeclarativeBase):
 
     #}
 
-class ComponentModel(DeclarativeBase):
+class Component(DeclarativeBase):
     __tablename__ = 'components'
 
     #{ Columns
 
     id = Column(Integer, primary_key=True)
 
-    ctype_id = Column(Integer, ForeignKey('componenttypes.id')
+    ctype_id = Column(Integer, ForeignKey('componenttypes.id'))
 
     ctype = relation(ComponentType, backref=backref('components', order_by=id))
 
