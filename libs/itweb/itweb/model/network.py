0f2a42b387d17601953dc8d54296bca56501484d
Josh <jokajak@gmail.com>
2011-05-09 09:01:34 -0400

    update network models to have relationships

--- libs/itweb/itweb/model/network.py
+++ libs/itweb/itweb/model/network.py
@@ -5,7 +5,7 @@ from sqlalchemy import *
 from sqlalchemy.orm import mapper, relation
 from sqlalchemy import Table, ForeignKey, Column
 from sqlalchemy.types import Integer, Unicode, DateTime
-#from sqlalchemy.orm import relation, backref
+from sqlalchemy.orm import relation, backref
 
 from itweb.model import DeclarativeBase, metadata, DBSession
 from itweb.lib.history_meta import VersionedMeta, VersionedListener
@@ -37,7 +37,9 @@ class IPAddress(DeclarativeBase):
 
     mac = Column(String(100), nullable=True)
 
-    network = Column(Integer, ForeignKey('networks.id'))
+    network_id = Column(Integer, ForeignKey('networks.id'))
+
+    network = relation(Network, backref=backref('ips', order_by=id))
 
     #}
 
@@ -45,7 +47,7 @@ class DNSEntry(DeclarativeBase):
     __metaclass__ = VersionedMeta
     __tablename__ = 'dnsentries'
     __table_args__ = (
-            UniqueConstraint('ipaddr', 'entry', 'type'),
+            UniqueConstraint('ipaddr_id', 'entry', 'type'),
             None,
             {}
             )
@@ -54,7 +56,9 @@ class DNSEntry(DeclarativeBase):
 
     id = Column(Integer, primary_key=True)
 
-    ipaddr = Column(Integer, ForeignKey('ips.id'))
+    ipaddr_id = Column(Integer, ForeignKey('ips.id'))
+
+    ipaddr    = relation(IPAddress, backref=backref('dnsentries', order_by=id))
 
     entry = Column(String(255), nullable=False)
 
